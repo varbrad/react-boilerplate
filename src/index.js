0fbc53bf7baf4997fa724307ca1833d84ccd1a8e
Brad Davies <varbrad@users.noreply.github.com>
2019-04-13 23:52:41 +0100

    Add optional chaining syntax support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
 const App = () => (
   <div>
     <p>Hello!</p>
   </div>
-)
+);
 
-render(<App />, null)
+render(<App />, null);
+
+const data = { maybe: [1, 2, 3] };
+
+console.log(data.maybe?.[0]);
